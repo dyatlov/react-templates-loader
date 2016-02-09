ea9aeb5e332886107fd08a7aace9ee43ca331c4e
Vitaly Dyatlov <md.xytop@gmail.com>
2016-02-09 16:38:10 +0200

    changed target version option

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-var reactTemplates = require('react-templates/src/reactTemplates');
+var reactTemplates = require('react-templates/src/reactTemplates').;
 var url = require('url');
 var queryString = require('querystring');
 
 module.exports = function(source) {
 	var options = queryString.parse(url.parse(this.query).query);
+	options['targetVersion'] = "0.14.0";
 	this.cacheable && this.cacheable();
 	return reactTemplates.convertTemplateToReact(source, options);
 };
